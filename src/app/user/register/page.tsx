420450c2ecdc2e8e25ec1e332a9f1b527d53401c
Luiz Eduardo <luizeduardobjr@outlook.com>
2024-03-11 16:52:27 -0300

    #16_register

---
+++ src/app/user/register/page.tsx
@@ -0,0 +1,9 @@
+import { Register } from "@/components/Register";
+
+export default function UserRegister() {
+    return (
+    <div className="flex flex-col items-center justify-center h-screen">
+        <Register />
+    </div>
+    );
+}
