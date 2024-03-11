5effd974c9b0f8c3bc0dd170155140de564af680
Luiz Eduardo <luizeduardobjr@outlook.com>
2024-03-11 16:51:34 -0300

    #13_signoptions

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,16 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${poppins.variable} ${dancing.variable} font-sans bg-gray-950 text-gray-100`}>{children}</body>
+      <body className={`${poppins.variable} ${dancing.variable} font-sans bg-gray-950 text-gray-100`}>
+        <div className="flex flex-col items-center justify-center h-screen">
+          <a href="/" className="mt-16 text-5xl font-bold font-alt">The best youtube video of "All time"</a>
+          <div className="mt-10">
+            <a href="/user/login" className="bg-gray-900 text-white font-semibold py-2 px-4 rounded-md mr-4 mb-2 hover:bg-gray-800">Sign in</a>
+            <a href="/user/register" className="bg-gray-900 text-white font-semibold py-2 px-4 rounded-md mb-2 hover:bg-gray-800">Sign up</a>
+          </div>
+          {children}
+        </div>        
+      </body>
     </html>
   )
 }
