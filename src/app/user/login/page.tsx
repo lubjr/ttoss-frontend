daa1e6b8ee9aeed59802ad5b181ba8f880e12fd4
Luiz Eduardo <luizeduardobjr@outlook.com>
2024-03-11 16:52:16 -0300

    #15_login

---
+++ src/app/user/login/page.tsx
@@ -0,0 +1,9 @@
+import { Login } from "@/components/Login";
+
+export default function UserLogin() {
+    return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <Login />
+    </div> 
+    );
+}
