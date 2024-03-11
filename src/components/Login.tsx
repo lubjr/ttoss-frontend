c000475eeebdc7b64dba182b9fcf92ce8974cfaf
Luiz Eduardo <luizeduardobjr@outlook.com>
2024-03-11 16:52:46 -0300

    #17_logincomponent

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -5,15 +5,15 @@ export function Login() {
         <form>
           <div className="mb-4">
             <label htmlFor="email" className="block text-gray-700 font-semibold mb-2">Email</label>
-            <input type="email" id="email" name="email" placeholder="Digite seu email" className="w-full border rounded-md py-2 px-3 text-gray-700"/>
+            <input type="email" id="email" name="email" placeholder="Type your e-mail" className="w-full border rounded-md py-2 px-3 text-gray-700"/>
           </div>
           <div className="mb-4">
-            <label htmlFor="password" className="block text-gray-700 font-semibold mb-2">Senha</label>
-            <input type="password" id="password" name="password" placeholder="Digite sua senha" className="w-full border rounded-md py-2 px-3 text-gray-700"/>
+            <label htmlFor="password" className="block text-gray-700 font-semibold mb-2">Password</label>
+            <input type="password" id="password" name="password" placeholder="Type your passwotd" className="w-full border rounded-md py-2 px-3 text-gray-700"/>
           </div>
-          <button type="submit" className="w-full bg-gray-200 text-zinc-900 font-semibold py-2 px-4 rounded-md hover:bg-gray-300">Entrar</button>
+          <button type="submit" className="w-full bg-gray-200 text-zinc-900 font-semibold py-2 px-4 rounded-md hover:bg-gray-300">Enter</button>
         </form>
       </div>
     </div>
-    )
+    );
 }
