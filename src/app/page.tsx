2174bc4cfdbe6fbf043f668e8f44f9e0d15f154c
Luiz Eduardo <luizeduardobjr@outlook.com>
2024-03-11 01:30:58 -0300

    #11_loginpage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { Login } from "@/components/Login";
 import { Chosen } from "../components/Chosen";
 //import { User, AtSign, Key } from 'lucide-react'
 
@@ -6,7 +7,8 @@ export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center h-screen">
       <h1 className="text-5xl font-bold mb-14 font-alt">The best youtube video of "All time"</h1>
-      <Chosen />
+      {/*<Chosen />*/}
+      <Login />
     </div>
   );
 }
---
+++ src/components/Login.tsx
@@ -0,0 +1,19 @@
+export function Login() {
+    return (
+    <div className="flex justify-center items-center">
+      <div className="bg-white p-8 rounded shadow-md w-80">
+        <form>
+          <div className="mb-4">
+            <label htmlFor="email" className="block text-gray-700 font-semibold mb-2">Email</label>
+            <input type="email" id="email" name="email" placeholder="Digite seu email" className="w-full border rounded-md py-2 px-3 text-gray-700"/>
+          </div>
+          <div className="mb-4">
+            <label htmlFor="password" className="block text-gray-700 font-semibold mb-2">Senha</label>
+            <input type="password" id="password" name="password" placeholder="Digite sua senha" className="w-full border rounded-md py-2 px-3 text-gray-700"/>
+          </div>
+          <button type="submit" className="w-full bg-gray-200 text-zinc-900 font-semibold py-2 px-4 rounded-md hover:bg-gray-300">Entrar</button>
+        </form>
+      </div>
+    </div>
+    )
+}
