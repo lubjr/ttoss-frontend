2c8939f13ac6f4d8fa712ae1d56938a1e93d98c3
Luiz Eduardo <luizeduardobjr@outlook.com>
2024-03-11 22:33:06 -0300

    #20_attlayout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,8 +21,8 @@ export default function RootLayout({
         <div className="flex flex-col items-center justify-center h-screen">
           <a href="/" className="mt-16 text-5xl font-bold font-alt">The best youtube video of "All time"</a>
           <div className="mt-10">
-            <a href="/user/login" className="bg-gray-900 text-white font-semibold py-2 px-4 rounded-md mr-4 mb-2 hover:bg-gray-800">Sign in</a>
-            <a href="/user/register" className="bg-gray-900 text-white font-semibold py-2 px-4 rounded-md mb-2 hover:bg-gray-800">Sign up</a>
+            <a href="" className="bg-gray-900 text-white font-semibold py-2 px-4 rounded-md mr-4 mb-2 hover:bg-gray-800">Sign in</a>
+            <a href="" className="bg-gray-900 text-white font-semibold py-2 px-4 rounded-md mb-2 hover:bg-gray-800">Sign up</a>
           </div>
           {children}
         </div>        
